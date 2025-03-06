40c10bf5be5003f71a46963437b2f71a5d07835c
K-Divyadharshini <divyadharshini252006@gmail.com>
2025-03-06 15:09:11 +0530

    Create Day 4-Task2.java

---
+++ Day 4-Task2.java
@@ -0,0 +1,33 @@
+interface Playable
+{
+void play();    
+}
+
+ class Guitar implements Playable
+{
+    @Override 
+    public void play()
+    {
+        System.out.println("** Play Guitar **");
+    }
+}
+class Piano implements Playable
+{
+    @Override
+    public void play()
+    {
+        System.out.println("** Play Piano **");
+    }
+}
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Playable g=new Guitar();
+        Playable p=new Piano();
+        g.play();
+        p.play();
+    }
+}
+
